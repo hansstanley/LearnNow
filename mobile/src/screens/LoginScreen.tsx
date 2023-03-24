917981e418e56d14e53bd79a27d6db0200c54eb2
hansstanley <nusstuhss@gmail.com>
2023-03-24 09:00:52 +0800

    Add register UI

--- mobile/src/screens/LoginScreen.tsx
+++ mobile/src/screens/LoginScreen.tsx
@@ -1,4 +1,12 @@
-import {Button, Center, Heading, Input, Text, VStack} from 'native-base';
+import {
+  Button,
+  Center,
+  Heading,
+  HStack,
+  Input,
+  Text,
+  VStack,
+} from 'native-base';
 import {useState} from 'react';
 import {useStoreActions} from '../features/auth/auth';
 import {login} from '../services/api';
@@ -7,10 +15,15 @@ export default function LoginScreen() {
   const setToken = useStoreActions(actions => actions.setToken);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [register, setRegister] = useState(false);
 
   const handleUsernameChange = (text: string) => setUsername(text);
 
   const handlePasswordChange = (text: string) => setPassword(text);
+
+  const handlePasswordConfirmChange = (text: string) =>
+    setPasswordConfirm(text);
 
   const handleLogin = () => {
     login(username, password)
@@ -22,30 +35,50 @@ export default function LoginScreen() {
       });
   };
 
+  const handleRegister = () => {};
+
+  const toggleRegister = () => {
+    setRegister(!register);
+  };
+
   return (
     <Center bg="primary.100" flex={1}>
-      <VStack space={5} alignItems="center">
+      <VStack
+        space={5}
+        alignItems="flex-start"
+        rounded="xl"
+        bg="light.50"
+        shadow={2}
+        padding={5}
+        width="80%">
         <Heading size="3xl">LearnNow</Heading>
-        <VStack
-          space={5}
-          alignItems="flex-start"
-          rounded="xl"
-          bg="light.50"
-          shadow={2}
-          padding={5}
-          width="80%">
+        <Input
+          placeholder="Username"
+          value={username}
+          onChangeText={handleUsernameChange}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChangeText={handlePasswordChange}
+        />
+        {register ? (
           <Input
-            placeholder="Username"
-            value={username}
-            onChangeText={handleUsernameChange}
+            type="password"
+            placeholder="Confirm password"
+            value={passwordConfirm}
+            onChangeText={handlePasswordConfirmChange}
           />
-          <Input
-            placeholder="Password"
-            value={password}
-            onChangeText={handlePasswordChange}
-          />
-          <Button onPress={handleLogin}>Login</Button>
-        </VStack>
+        ) : null}
+        <HStack space={2}>
+          <Button onPress={handleLogin}>
+            {register ? 'Register' : 'Login'}
+          </Button>
+          <Button variant="ghost" onPress={toggleRegister}>
+            {register ? 'Already have an account?' : "Don't have an account?"}
+          </Button>
+        </HStack>
       </VStack>
     </Center>
   );
