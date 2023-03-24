297cdc387b980afc13c6a02b196741404f6b5112
hansstanley <nusstuhss@gmail.com>
2023-03-24 21:10:07 +0800

    Add chapter progress

--- mobile/src/screens/ChapterScreen.tsx
+++ mobile/src/screens/ChapterScreen.tsx
@@ -1,13 +1,24 @@
-import {Box, ScrollView, Text, View} from 'native-base';
+import {Progress, ScrollView, Stack, Text, View} from 'native-base';
 import {useEffect, useState} from 'react';
 import {SectionList} from '../components/section';
+import {ProgressStore} from '../features/progress';
 import {getSections} from '../services/api';
 import {ChapterScreenProps} from '../types/navigation';
 import {Section} from '../types/section';
 
 export default function ChapterScreen({route}: ChapterScreenProps) {
   const {chapter} = route.params;
+  const progresses = ProgressStore.useStoreState(
+    state => state.progress.progresses,
+  );
   const [sections, setSections] = useState<Section[]>([]);
+
+  const completedSectionIds = progresses
+    .filter(p => p.isCompleted)
+    .map(p => p.sectionId);
+  const completedCount = sections.filter(s =>
+    completedSectionIds.includes(s.pk),
+  ).length;
 
   useEffect(() => {
     getSections(chapter.pk)
@@ -18,15 +29,22 @@ export default function ChapterScreen({route}: ChapterScreenProps) {
   return (
     <View>
       <ScrollView>
-        <Box p={5}>
-          <Text
-            mb={5}
-            _light={{color: 'primary.800'}}
-            _dark={{color: 'primary.200'}}>
+        <Stack space={5} p={5}>
+          <Text _light={{color: 'primary.800'}} _dark={{color: 'primary.200'}}>
             {chapter.fields.description}
           </Text>
+          <Stack space={2} p={4} rounded="2xl" bg="light.50" shadow={2}>
+            <Text>
+              {completedCount} of {sections.length} sections read
+            </Text>
+            <Progress
+              value={
+                sections.length && (completedCount * 100) / sections.length
+              }
+            />
+          </Stack>
           <SectionList sections={sections} />
-        </Box>
+        </Stack>
       </ScrollView>
     </View>
   );
